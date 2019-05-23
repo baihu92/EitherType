112d17f53e499a6cae8bac93932d79a983866d2e
baihu92 <baihu92@gmail.com>
2019-05-23 12:11:49 +0300

    Implemented basic functionality of the either type.

---
+++ example/src/main.dart
@@ -0,0 +1,34 @@
+import 'package:either/either.dart';
+import 'dart:math';
+
+// Some user class.
+class User {
+  final String name;
+
+  User(this.name);
+}
+
+// Some error class.
+class ServerError {
+  final int code;
+
+  ServerError(this.code);
+}
+
+main() async {
+  // There is some method that will return the user or error.
+  await getUser()
+    ..either((ServerError error) {
+      // Add a handler for the error.
+      print("Error: ${error.code}");
+    }, (User user) {
+      // Add a handler to get the user.
+      print("User: ${user.name}");
+    });
+}
+
+// And the return of either looks like this.
+// According to the agreement, the right part stores the result of success,
+// left is wrong.
+Future<Either<ServerError, User>> getUser() async =>
+    (Random().nextBool()) ? Right(User("Bob")) : Left(ServerError(500));
---
+++ lib/src/either.dart
@@ -0,0 +1,44 @@
+/// Signature of callbacks that have no arguments and return right or left value.
+typedef Callback<T> = void Function(T value);
+
+/// Represents a value of one of two possible types (a disjoint union).
+/// Instances of [Either] are either an instance of [Left] or [Right].
+/// FP Convention dictates that:
+///   [Left] is used for "failure".
+///   [Right] is used for "success".
+abstract class Either<L, R> {
+  Either() {
+    if (!isLeft && !isRight)
+      throw Exception('The ether should be heir Left or Right.');
+  }
+
+  /// Represents the left side of [Either] class which by convention is a "Failure".
+  bool get isLeft => this is Left<L, R>;
+
+  /// Represents the right side of [Either] class which by convention is a "Success"
+  bool get isRight => this is Right<L, R>;
+
+  void either(Callback<L> fnL, Callback<R> fnR) {
+    if (isLeft) {
+      final left = this as Left<L, R>;
+      fnL(left.value);
+    }
+
+    if (isRight) {
+      final right = this as Right<L, R>;
+      fnR(right.value);
+    }
+  }
+}
+
+class Left<L, R> extends Either<L, R> {
+  final L value;
+
+  Left(this.value);
+}
+
+class Right<L, R> extends Either<L, R> {
+  final R value;
+
+  Right(this.value);
+}
